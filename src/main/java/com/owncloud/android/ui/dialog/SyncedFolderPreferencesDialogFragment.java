0133f6867d64c72db41ee7713ba8e48a01762626
AndyScherzinger <info@andy-scherzinger.de>
2017-05-30 23:23:44 +0200

    hide delete button for unpersisted custom folders

--- src/main/java/com/owncloud/android/ui/dialog/SyncedFolderPreferencesDialogFragment.java
+++ src/main/java/com/owncloud/android/ui/dialog/SyncedFolderPreferencesDialogFragment.java
@@ -49,6 +49,8 @@ import com.owncloud.android.utils.DisplayUtils;
 
 import java.io.File;
 
+import static com.owncloud.android.datamodel.SyncedFolderDisplayItem.UNPERSISTED_ID;
+
 /**
  * Dialog to show the preferences/configuration of a synced folder allowing the user to change the different parameters.
  */
@@ -130,7 +132,11 @@ public class SyncedFolderPreferencesDialogFragment extends DialogFragment {
     private void setupDialogElements(View view) {
 
         if (mSyncedFolder.getType() > MediaFolder.CUSTOM) {
+            // hide local folder chooser and delete for non-custom folders
             view.findViewById(R.id.local_folder_container).setVisibility(View.GONE);
+            view.findViewById(R.id.delete).setVisibility(View.GONE);
+        } else if (mSyncedFolder.getId() <= UNPERSISTED_ID) {
+            // Hide delete for unpersisted custom folders
             view.findViewById(R.id.delete).setVisibility(View.GONE);
         }
 
